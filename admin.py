6291ac396cac20474237a29db712ef93274d3041
Dhananjay Ranate <dhananjayranate625@gmail.com>
2026-04-09 10:47:14 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,20 +1,31 @@
-from django.contrib import admin
-from .models import Question, Choice, Submission
+from django.db import models
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 1
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
 
-class QuestionInline(admin.TabularInline):
-    model = Question
-    extra = 1
+    def __str__(self):
+        return self.question_text
 
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
 
-class LessonAdmin(admin.ModelAdmin):
-    inlines = [QuestionInline]
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
 
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
-admin.site.register(Submission)
+    def __str__(self):
+        return self.choice_text
+
+
+class Enrollment(models.Model):
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return f"Submission {self.id}"
