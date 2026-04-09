a305c93d621bd5724622446bbb9c842fe328ece9
Dhananjay Ranate <dhananjayranate625@gmail.com>
2026-04-09 17:01:30 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,29 +3,14 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.question_text
-
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.choice_text
-
-
 class Enrollment(models.Model):
     user = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.user
-
 
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
-
-    def __str__(self):
-        return f"Submission {self.id}"
