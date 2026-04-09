f7e24c901849ddcf7d55e1ed2e8611883ade58e0
Dhananjay Ranate <dhananjayranate625@gmail.com>
2026-04-09 10:49:34 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,31 +1,31 @@
-from django.contrib import admin
-from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
 
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 1
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
 
 
-class QuestionInline(admin.TabularInline):
-    model = Question
-    extra = 1
+class Enrollment(models.Model):
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-    list_display = ('question_text',)
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 
-
-class LessonAdmin(admin.ModelAdmin):
-    inlines = [QuestionInline]
-    list_display = ('title',)
-
-
-admin.site.register(Course)
-admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
-admin.site.register(Learner)
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
-admin.site.register(Submission)
+    def __str__(self):
+        return f"Submission {self.id}"
